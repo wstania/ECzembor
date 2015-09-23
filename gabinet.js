837258f0b3be9a55c34977409081a8b04083bc03
wstania <w.stania@wp.pl>
2015-09-23 16:45:44 +0200

    add class after scroll

--- gabinet.js
+++ gabinet.js
@@ -5,19 +5,47 @@ $(document).ready(function(){
     $('.feature.bordered.mb30 .uppercase').mouseover(function(){
         $(this).addClass("hvr-float-shadow");
     });
-   $('a[href^="#"]').on('click', function(e) {
 
-        var target = $( $(this).attr('href') );
 
-        if( target.length ) {
+
+
+        $(document).on("scroll", onScroll);
+
+        $('a[href^="#"]').on('click', function (e) {
             e.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
+            $(document).off("scroll");
 
-    });
+            $('a').each(function () {
+                $(this).removeClass('active');
+            })
+            $(this).addClass('active');
+
+            var target = this.hash;
+            $target = $(target);
+            $('html, body').stop().animate({
+                'scrollTop': $target.offset().top+2
+            }, 900, 'swing', function () {
+                window.location.hash = target;
+                $(document).on("scroll", onScroll);
+            });
+        });
+
+
+    function onScroll(event){
+        var scrollPosition = $(document).scrollTop();
+        $('nav a').each(function () {
+            var currentLink = $(this);
+            var refElement = $(currentLink.attr("href"));
+            if (refElement.position().top <= scrollPosition && refElement.position().top + refElement.height() > scrollPosition) {
+                $('ul.menu').find('a').removeClass("active");
+                currentLink.addClass("active");
+            }
+            else{
+                currentLink.removeClass("active");
+            }
+        });
+    }
+
 
 });
-
 
